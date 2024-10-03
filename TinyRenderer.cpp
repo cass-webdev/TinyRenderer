2bb6e1c40fe74c993c30f66fe97ae5ab4026d8e9
cramondev <christian.ramon.dev@gmail.com>
2024-10-02 22:08:02 -0600

    feat: basic line drawing attempt 2
    adding some exploratory comments
    lines can be drawn but still flawed
    since they can only be drawn
    from left to right

--- TinyRenderer.cpp
+++ TinyRenderer.cpp
@@ -8,21 +8,35 @@ using namespace std;
 
 const TGAColor white = { 255, 255, 255, 255 };
 const TGAColor blue  = { 255, 0  , 0  , 255 }; // this is BRGA instead of RGBA like I expected - why?
+const TGAColor red   = { 0  , 255, 0  , 255 }; // this is BRGA instead of RGBA like I expected - why?
 
-// Thanks to https://www.geeksforgeeks.org/passing-by-pointer-vs-passing-by-reference-in-cpp/
-// I'm guessing pass by ref is the generally safer option when accessing members of an instance in C++
-// Probably should do pass by ref because we're not trying to modify internals of the TGAImage class directly? (does that make sense?)
+// Thanks to https://www.geeksforgeeks.org/passing-by-pointer-vs-passing-by-reference-in-cpp/ for some extra knowledge
+// Second attempt - this is supposed to be erroneous as demonstrated, so try and figure out what's wrong.
+/*
+   Problems:
+   1. What if x0 (start x) is greater than x1 (end x)?
+     - Line will simply not render
+     - This algorithm cannot handle coordinates that result in a straight vertical line.
+     - Curiously, we can only render lines that fall within a ~178 deg angle facing right
+   2. "int y" definition is affected by truncation
+
+   Something to note:
+   * The result of drawing a line segment should not depend on the order of points: the (a,b) line segment should be exactly the same as the (b,a) line segment.
+*/
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-    for (float t = 0.; t < 1.; t += .01) {
-        int x = x0 + (x1 - x0) * t;
-        int y = y0 + (y1 - y0) * t;
+    for (int x = x0; x <= x1; x++) {
+        float t = (x - x0) / (float)(x1 - x0);
+        int y = y0 * (1. - t) + y1 * t;
         image.set(x, y, color);
     }
 }
 
 int main(int argc, char** argv) {
     TGAImage image(100, 100, TGAImage::RGB);
-    line(41, 45, 59, 55, image, blue);
+    line(13, 20, 80, 40, image, white);
+    line(20, 13, 40, 80, image, red);
+    line(80, 40, 13, 20, image, blue);
+    line(13, 80, 14, 13, image, white);
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
     return 0;
